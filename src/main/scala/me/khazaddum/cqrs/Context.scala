d8c900a4f1351f07ededfa131f8860f85a85b68d
km3ll <kmell@protonmail.com>
2021-02-04 14:31:32 -0500

    Load configuration file to context

--- src/main/scala/me/khazaddum/cqrs/Context.scala
+++ src/main/scala/me/khazaddum/cqrs/Context.scala
@@ -1,5 +1,17 @@
 package me.khazaddum.cqrs
 
+import com.typesafe.config.{ Config, ConfigFactory }
+
 trait Context {
+
+  val config: Config
+  val token: String
+
+}
+
+case class LocalContext() extends Context {
+
+  val config: Config = ConfigFactory.load( "me.khazad-dum.cqrs" )
+  val token: String = config.getString( "token" )
 
 }
